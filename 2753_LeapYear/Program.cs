867ad1dffae1354d4cfbc0b63b26c0207fa917fe
MunSeongJun5133 <ans1332@naver.com>
2025-06-13 23:18:09 +0900

    초기 커밋.

---
+++ 2753_LeapYear/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.UInt16 lp = System.UInt16.Parse(Console.ReadLine());
+            if (lp % 4 == 0 && (lp % 100 != 0 || lp % 400 == 0))
+                Console.WriteLine(1);
+            else
+                Console.WriteLine(0);
+        }
+    }
+}
